9beceb3564bea45373288155113c121898adce59
e232997 <e232997@edu.sbl.ch>
2022-11-16 14:45:05 +0100

    change color of player

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function handleKeyPress(ev) {
 const player = document.querySelector("#player")
 player.setAttribute("cx", 100 * Math.random())
 player.setAttribute("cy", 100 * Math.random())
+} else if (ev.key === "r"){
+    player.setAttribute("fill", "red")
 }
 }
 
